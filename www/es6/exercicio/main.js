8ca8237d67a0c0126846bad66341036c6ed36fb7
Augusto <leandro.augusto.mello@proton.me>
2025-03-20 11:09:44 -0300

    Update

---
+++ www/es6/exercicio/main.js
@@ -0,0 +1,14 @@
+const alunos = [
+ { nome: "João", nota: 4.5 },
+ { nome: "Maria", nota: 9.0 },
+ { nome: "Pedro", nota: 7.5 },
+ { nome: "Ana", nota: 5.0 },
+ { nome: "Lucas", nota: 6.0 }
+];
+
+function alunosAprovados(alunos) {
+ return alunos.filter(aluno => aluno.nota >= 6);
+}
+
+const aprovados = alunosAprovados(alunos);
+console.log(aprovados);
