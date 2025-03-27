ce07cafb7972c397f6a89b97b8059e755109a8f2
Augusto <leandro.augusto.mello@proton.me>
2025-03-27 09:25:47 -0300

    Update

--- www/typescript/exercicio/main.ts
+++ www/typescript/exercicio/main.ts
@@ -1,16 +1,7 @@
-const multiplica = (a: number, b: number): void => console.log(a * b);
-const dizOla = (nome: string): void => console.log("Olá " + nome);
-
-function multiplicaDois(a: number, b: number): void {
- return console.log(a * b);
+function multiplicaDois(a: number, b: number) {
+ return a * b;
 }
 
-function dizOlaDois(nome: string): void {
- return console.log("Olá " + nome);
+function dizOlaDois(nome: string) {
+ return "Olá " + nome;
 }
-
-multiplica(2, 2);
-dizOla('Carlos');
-
-multiplicaDois(8, 8);
-dizOlaDois('Roberto');
